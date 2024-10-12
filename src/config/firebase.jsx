60df44e61736e96a55871f9cb400fc5ffe64a2c4
jyassien <jihadumuhammed@gmail.com>
2024-10-12 15:33:26 -0600

    Firebase setup and configuration.

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,8 +1,8 @@
-require("dotenv").config();
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,9 +16,10 @@ const firebaseConfig = {
   messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
   measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
-
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const GoogleProvider = new GoogleAuthProvider();
