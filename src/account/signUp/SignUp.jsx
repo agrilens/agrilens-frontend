cac98d10d7acd5a0868e9936551366349ad9c4e3
jyassien <jihadumuhammed@gmail.com>
2024-10-12 15:30:08 -0600

    Account login, logout, and signup simple set up with Firebase Authentication.

--- src/account/signUp/SignUp.jsx
+++ src/account/signUp/SignUp.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import {
+  useAccountContext,
+  useAccountUpdateContext,
+} from "../../contexts/AccountContext";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const { userType } = useAccountContext();
+  const { updateUserType } = useAccountUpdateContext();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUp = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log("currentUser: ", auth?.currentUser);
+    } catch (error) {
+      console.log("currentUser: ", auth?.currentUser);
+      console.error("Sign up error", error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button onClick={signUp}>Sign Up</button>
+      </div>
+    </div>
+  );
 }
