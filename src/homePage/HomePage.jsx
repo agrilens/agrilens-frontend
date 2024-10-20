4be9a289cc058fdab2c85050061db8f55cd95bc3
jyassien <jihadumuhammed@gmail.com>
2024-10-20 03:16:51 -0600

    Adds HomePage id and className.

--- src/homePage/HomePage.jsx
+++ src/homePage/HomePage.jsx
@@ -12,8 +12,8 @@ export default function HomePage() {
   const { updateUserType } = useAccountUpdateContext();
 
   return (
-    <section id="">
-      <div>
+    <section id="homePage">
+      <div className="home-container">
         <UploadImage />
       </div>
     </section>
