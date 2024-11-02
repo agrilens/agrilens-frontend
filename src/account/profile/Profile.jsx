6dfb6aee7fde0587de2f76a55f2b5c2a763531ae
jyassien <jihadumuhammed@gmail.com>
2024-11-02 17:40:56 -0600

    removes old profile/ directory.

--- src/account/profile/Profile.jsx
+++ src/account/profile/Profile.jsx
@@ -139,3 +139,22 @@ export const Profile = () => {
 };
 
 export default Profile;
+
+/* import React from "react";
+import {
+  useAccountContext,
+  useAccountUpdateContext,
+} from "../contexts/AccountContext";
+
+export default function Profile() {
+  const { userType } = useAccountContext();
+  const { updateUserType } = useAccountUpdateContext();
+
+  return (
+    <div>
+      <div>Profile : {userType}</div>
+      <button onClick={updateUserType}>Click to change user type</button>
+    </div>
+  );
+}
+*/
