e3c4e9132584305aba287f4ae04a22b33305e02d
jyassien <jihadumuhammed@gmail.com>
2024-11-02 01:08:09 -0600

    updates structure of the App view.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NotFound from "./common/NotFound";
 import Profile from "./profile/Profile";
 import LoadingSpinner from "./common/LoadingSpinner";
 import Dashboard from "./features/dashboard/Dashboard";
+import Footer from "./footer/Footer";
 
 import "bootstrap/dist/css/bootstrap.css";
 // import "bootstrap/dist/css/bootstrap.min.css"; // Recommended in production for optimized page loading.
@@ -24,24 +25,31 @@ function App() {
   // { hash: "", key: "", pathname: "", search: "", state: "" }
   return (
     <div className="App">
-      <AccountProvider>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="*" element={<NotFound />} />
+      <AccountProvider className="">
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="*" element={<NotFound />} />
 
-          {/* Route Nesting Framework. We can also create a Layout component to make it visible to every component. */}
-          {/* <Route path="/nest">
+            {/* Route Nesting Framework. We can also create a Layout component to make it visible to every component. */}
+            {/* <Route path="/nest">
           <Route path="home" element={<HomePage/>}/>
           <Route path="about" element={<AboutUs/>}/>
         </Route> */}
-        </Routes>
+          </Routes>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
       </AccountProvider>
     </div>
   );
