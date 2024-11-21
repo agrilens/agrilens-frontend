e1d7bddd9194125c2c236f3c27084dd2fc4fb57c
bednie <88800117+bednie@users.noreply.github.com>
2024-11-21 14:54:49 -0700

    Update LegalTerms.jsx

--- src/common/LegalTerms.jsx
+++ src/common/LegalTerms.jsx
@@ -61,13 +61,10 @@ export const LegalTerms = () => {
             </ul>
             <h3>5. Intellectual Property</h3>
             <p>
-              Copyright © 2024 AgriLens Project Team - Metropolitan State University of Denver.
+              This work created by AgriLens Team members Jihadu Yassien, Jessica Nguyen, Luke Farchione, Jacqueline Hernandez, and Blair Ednie. 
             </p>
             <p>
-              Created by Jihadu Yassien, Jessica Nguyen, Luke Farchione, Jacqueline Hernandez, and Blair Ednie. 
-            </p>
-            <p>
-              This work is licensed under Creative Commons Attribution-ShareAlike 4.0 International.
+              <p xmlns:cc="http://creativecommons.org/ns#" ><a rel="cc:attributionURL" href="https://agrilens-web.web.app/">This work</a> by <span property="cc:attributionName">AgriLens Team</span> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">Creative Commons Attribution-ShareAlike 4.0 International<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>
               To view a copy of this license, visit https://creativecommons.org/licenses/by-sa/4.0/
               All content, features, and functionality of this application are the property of the AgriLens Project Team and are protected by copyright laws.
             </p>
